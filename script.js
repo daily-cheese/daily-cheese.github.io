502c3dd519f53620445bc109c18b5a7266e808e1
Sam <sam@moocows.co.uk>
2024-01-29 17:16:15 +0000

    mmm i dont even know what i did

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// Time
-function timeUpdate() {
-    // Get date object and find time values
-    let d = new Date();
-    let timeArr = [d.getHours(), d.getMinutes(), d.getSeconds()];
+// // Time
+// function timeUpdate() {
+//     // Get date object and find time values
+//     let d = new Date();
+//     let timeArr = [d.getHours(), d.getMinutes(), d.getSeconds()];
+//
+//     // Get text element to change
+//     let element = document.getElementById("timeText");
+//     let newH = timeArr[0];
+//     let newM = timeArr[1];
+//     let newS = timeArr[2];
+//
+//     // If values are single digit, add leading 0
+//     if (timeArr[0] < 10) {
+//         newH = `0${newH}`;
+//     }
+//     if (timeArr[1] < 10){
+//         newM = `0${newM}`;
+//     }
+//     if (timeArr[2] < 10) {
+//         newS = `0${newS}`;
+//     }
+//
+//     // update element
+//     element.innerHTML = `It is ${newH}:${newM}:${newS}`;
+// }
+//
+// setInterval(timeUpdate, 500);
 
-    // Get text element to change
-    let element = document.getElementById("timeText");
-    let newH = timeArr[0];
-    let newM = timeArr[1];
-    let newS = timeArr[2];
-
-    // If values are single digit, add leading 0
-    if (timeArr[0] < 10) {
-        newH = `0${newH}`;
-    }
-    if (timeArr[1] < 10){
-        newM = `0${newM}`;
-    }
-    if (timeArr[2] < 10) {
-        newS = `0${newS}`;
-    }
-
-    // update element
-    element.innerHTML = `It is ${newH}:${newM}:${newS}`;
+function showText1() {
+    let txt = document.getElementById("text1");
+    txt.style.opacity = 1;
 }
-
-setInterval(timeUpdate, 500);
-
-// Arrow up down anim
